cbe77d6977d59c81bb3e3e2096b6395a77994765
David Tracy <davidptracy@gmail.com>
2015-05-03 22:25:35 -0400

    adjust mapping on roll

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ io.sockets.on('connection', function (socket){
 
 		oculusOrientationVals[0] = Math.floor(map_range(pitch, -.7, .7, 175, 5));
 		oculusOrientationVals[1] = Math.floor(map_range(yaw, -.7, .7, 175, 5));
-		oculusOrientationVals[2] = Math.floor(map_range(roll, -.7, .7, 175, 5));
+		oculusOrientationVals[2] = Math.floor(map_range(roll, -.7, .7, 5, 175));
 
 		console.log(oculusOrientationVals);
 
